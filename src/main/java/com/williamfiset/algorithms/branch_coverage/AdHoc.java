e7b7a3c8987e569016e83532d11bb9ac15fb8432
Yosef A <61015855+yamean3567@users.noreply.github.com>
2023-02-20 13:52:40 +0100

    Feat: branch coverage for lineSegmentLineSegmentIntersection using ad-hoc tool. Refs #28
    Feat: branch coverage by manual instrumentation. Resolves #5
    
    Update: add invalid id checking, and reformat code. Refs #5

---
+++ src/main/java/com/williamfiset/algorithms/branch_coverage/AdHoc.java
@@ -0,0 +1,76 @@
+package com.williamfiset.algorithms.branch_coverage;
+
+import java.lang.StringBuilder;
+
+/**
+ * Class provides functions that can be used for manual instrumentation of
+ * code, and reporting branch coverage.
+ */
+public class AdHoc {
+    /** Holds whether branch is covered or not */
+    private boolean[] coveredBranch;
+
+    /** Total number of branches that can be covered */
+    private int numberOfBranches;
+
+    /** 
+     * Constructs an object of this class with given number of branches
+     * 
+     * @param numberOfBranches the number of branches that can be covered
+     */
+    public AdHoc(int numberOfBranches) {
+        this.coveredBranch = new boolean[numberOfBranches];
+        this.numberOfBranches = numberOfBranches;
+    }
+
+    /**
+     * Mark branch as covered
+     * 
+     * @param id the id of the branch that is marked as covered
+     * @throws IndexOutOfBoundsException if the given ID is invalid
+     */
+    public void coverBranch(int id) {
+        if (id < 0 || id >= numberOfBranches) {
+            throw new IndexOutOfBoundsException("Invalid branch ID: " + id);
+        }
+
+        coveredBranch[id] = true;
+    }
+
+    /**
+     * Get total branch coverage
+     * 
+     * @return the ratio of branches covered
+     */
+    public float branchCoverage() {
+        int coveredBranches = 0;
+
+        for (boolean b: coveredBranch) {
+            if (b) coveredBranches++;
+        }
+
+        return (float) coveredBranches / this.numberOfBranches;
+    }
+
+    /**
+     * Get branch report
+     * 
+     * @return report as a string, including whether each branch has been covered, and total
+     * branch coverage in percent
+     */
+    public String getReport() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("%-15s %-15s\n", "Branch ID", "Taken"));
+
+        for (int i = 0; i < this.numberOfBranches; ++i) {
+            String taken = coveredBranch[i] ? "Yes" : "No";
+
+            sb.append(String.format("%-15s %-15s\n", i, taken));
+        }
+
+        sb.append(String.format("\nBranch coverage: %.2f%%\n", branchCoverage() * 100));
+
+        return sb.toString();
+    }
+}
