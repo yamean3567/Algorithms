fc3e01779b9b730900c7f2ec7a0fb749aa28b982
Bength Pappila <bength@typo.local>
2023-02-20 11:46:17 +0100

    Refactor: Refactor function closesPair, reduce complexity. Resolves #21

--- src/main/java/com/williamfiset/algorithms/geometry/ClosestPairOfPoints.java
+++ src/main/java/com/williamfiset/algorithms/geometry/ClosestPairOfPoints.java
@@ -9,14 +9,17 @@ package com.williamfiset.algorithms.geometry;
 
 import static java.lang.Math.*;
 
+import java.lang.reflect.Type;
 import java.util.*;
 
 public class ClosestPairOfPoints {
 
   private static final double EPS = 1e-9;
 
-  public class PT {
+  public class PT implements Type{
     double x, y;
+
+    public PT(){}
 
     public PT(double xx, double yy) {
       x = xx;
@@ -50,6 +53,28 @@ public class ClosestPairOfPoints {
     }
   }
 
+  /**
+   * Search above or below the current point for potential closer points 
+   * */ 
+  private static Object[] searchPoints(TreeSet<PT> yWorkingSet, PT nextPoint, double d, boolean searchAbove, PT pt1prev, PT pt2prev) {
+    Object pt1 = pt1prev;
+    Object pt2 = pt2prev;
+    double bound = searchAbove ? nextPoint.y + d : nextPoint.y - d;
+    PT next = searchAbove ? yWorkingSet.higher(nextPoint) : yWorkingSet.lower(nextPoint);
+    while (next != null && (searchAbove ? next.y <= bound : next.y > bound)) {
+      double dist = nextPoint.dist(next);
+      if (dist < d) {
+        pt1 = nextPoint;
+        pt2 = next;
+        d = dist;
+      }
+      next = searchAbove ? yWorkingSet.higher(next) : yWorkingSet.lower(next);
+    }
+
+    return new Object[]{pt1, pt2, Double.valueOf(d)};
+  }
+
+
   // Finds the closest pair of points in a list of points
   public static PT[] closestPair(PT[] points) {
 
@@ -75,34 +100,25 @@ public class ClosestPairOfPoints {
         PT pt = points[xQueueFront++];
         yWorkingSet.remove(pt);
       }
+      
+      // Look at all the points in our working set with a y-coordinate
+      // above nextPoint.y but within a window of nextPoint.y + d
+      
+      Object[] pointsAndDist = new Object[3];
 
       // Look at all the points in our working set with a y-coordinate
       // above nextPoint.y but within a window of nextPoint.y + d
-      double upperBound = nextPoint.y + d;
-      PT next = yWorkingSet.higher(nextPoint);
-      while (next != null && next.y <= upperBound) {
-        double dist = nextPoint.dist(next);
-        if (dist < d) {
-          pt1 = nextPoint;
-          pt2 = next;
-          d = dist;
-        }
-        next = yWorkingSet.higher(next);
-      }
-
+      pointsAndDist = searchPoints(yWorkingSet, nextPoint, d, true, pt1, pt2);
+      pt1 = (PT) pointsAndDist[0];
+      pt2 = (PT)  pointsAndDist[1];
+      d = (Double) pointsAndDist[2];
       // Look at all the points in our working set with a y-coordinate
       // below nextPoint.y but within a window of nextPoint.y - d
-      double lowerBound = nextPoint.y - d;
-      next = yWorkingSet.lower(nextPoint);
-      while (next != null && next.y > lowerBound) {
-        double dist = nextPoint.dist(next);
-        if (dist < d) {
-          pt1 = nextPoint;
-          pt2 = next;
-          d = dist;
-        }
-        next = yWorkingSet.lower(next);
-      }
+      pointsAndDist = searchPoints(yWorkingSet, nextPoint, d, false, pt1, pt2);
+      pt1 = (PT) pointsAndDist[0];
+      pt2 = (PT)  pointsAndDist[1];
+      d = (Double) pointsAndDist[2];
+      
 
       // Duplicate/stacked points
       if (yWorkingSet.contains(nextPoint)) {
